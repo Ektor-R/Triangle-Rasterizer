7be8795169858e558c29a45081033165e6cd3f8e
Ektoras Rontos <rontekto@gmail.com>
2022-04-09 04:34:36 +0300

    Demos

---
+++ demo_gouraud.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+from datetime import datetime
+
+from src import functions
+
+data = np.load('src/Data/hw1.npy', allow_pickle=True)
+
+img = functions.render(
+    data[()]['verts2d'],
+    data[()]['faces'],
+    data[()]['vcolors'],
+    data[()]['depth'],
+    'gouraud'
+)
+
+cv2.imwrite(
+    'images/img-gouraud-' + datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.jpg',
+    cv2.cvtColor( (img*255).astype(np.uint8), cv2.COLOR_BGR2RGB)
+) 
