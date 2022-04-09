2920e07de2fcb4f0bdde9c3a33c41098b5db6e91
Ektoras Rontos <rontekto@gmail.com>
2022-04-09 04:53:01 +0300

    Execution time

--- demo_gouraud.py
+++ demo_gouraud.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2
 from datetime import datetime
+import time
 
 from src import functions
+
+startTime = time.time()
 
 data = np.load('src/Data/hw1.npy', allow_pickle=True)
 
@@ -17,4 +20,6 @@ img = functions.render(
 cv2.imwrite(
     'images/img-gouraud-' + datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.jpg',
     cv2.cvtColor( (img*255).astype(np.uint8), cv2.COLOR_BGR2RGB)
-) 
+)
+
+print("--- %s seconds ---" % (time.time() - startTime))
